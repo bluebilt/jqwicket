e53973151a0f5cd77d0c22aa23c5e3edc30c4dd7
max@javaforge.net <max@javaforge.net@57ee0337-2cae-a1d2-7317-812cb19e8380>
2010-12-01 13:09:25 +0000

    added gmap plugin implementation

---
+++ jqwicket/src/main/java/net/javaforge/jqwicket/ui/gmap/GMapBehavior.java
@@ -0,0 +1,60 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package net.javaforge.jqwicket.ui.gmap;
+
+import net.javaforge.jqwicket.JQHeaderContributionTarget;
+import net.javaforge.jqwicket.ui.JQComponentBehaivor;
+
+/**
+ * @author mkalina
+ * 
+ */
+public class GMapBehavior extends JQComponentBehaivor<GMapOptions> implements
+		IGMap {
+
+	private static final long serialVersionUID = 1L;
+
+	public GMapBehavior(String apiKey) {
+		this(new GMapOptions(apiKey));
+	}
+
+	public GMapBehavior(GMapOptions options) {
+		super(options);
+	}
+
+	/**
+	 * {@inheritDoc}
+	 * 
+	 * @see net.javaforge.jqwicket.IJQUIWidget#getName()
+	 */
+	public String getName() {
+		return JQ_COMPONENT_NAME;
+	}
+
+	/**
+	 * {@inheritDoc}
+	 * 
+	 * @see net.javaforge.jqwicket.ui.JQComponentBehaivor#contribute(net.javaforge.jqwicket.JQHeaderContributionTarget)
+	 */
+	@Override
+	public void contribute(JQHeaderContributionTarget target) {
+		target.addJavascriptResourceUrls("http://maps.google.com/maps?file=api&amp;v=2&amp;key="
+				+ this.options.getApiKey());
+		super.contribute(target);
+	}
+
+}
