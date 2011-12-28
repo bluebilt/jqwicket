91a380a6b2ab93e8a5c416db4b0bc4ed88cbb90e
max@javaforge.net <max@javaforge.net@57ee0337-2cae-a1d2-7317-812cb19e8380>
2011-12-28 09:29:39 +0000

    jquery version ugprade -> 1.7.1

--- jqwicket/src/main/java/com/google/code/jqwicket/JQContributionConfig.java
+++ jqwicket/src/main/java/com/google/code/jqwicket/JQContributionConfig.java
@@ -38,7 +38,7 @@ public class JQContributionConfig implements Serializable {
     };
 
     private static final CharSequence defaultJQueryCoreJsUrl = "http://ajax.googleapis.com/ajax/libs/" +
-            "jquery/1.7.0/jquery.min.js";
+            "jquery/1.7.1/jquery.min.js";
 
     private static final CharSequence defaultJQueryUiJsUrl = "http://ajax.googleapis.com/ajax/libs/" +
             "jqueryui/1.8.16/jquery-ui.min.js";
